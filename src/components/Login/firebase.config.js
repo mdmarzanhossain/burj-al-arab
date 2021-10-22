858f5dda5e31e7628a8b31928f2c1dfdf5c71d83
Marzan Hossain <33099914+Marzan1234eng@users.noreply.github.com>
2021-10-22 21:19:57 +0600

    Delete firebase.config.js

--- src/components/Login/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyATEUCTv4dBKxk0q-9SIyEffk7Inek9ezY",
-    authDomain: "burj-al-arab-70832.firebaseapp.com",
-    projectId: "burj-al-arab-70832",
-    storageBucket: "burj-al-arab-70832.appspot.com",
-    messagingSenderId: "596164254348",
-    appId: "1:596164254348:web:66bdff6127de97b60cf9a9"
-};
